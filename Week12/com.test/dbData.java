3f8d7f655303fbafa84136e750f40d8a4806aaf7
joo1124 <127066632+joo1124@users.noreply.github.com>
2024-05-30 16:53:12 +0900

    Create dbData.java

---
+++ Week12/com.test/dbData.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class dbData {
+	String[] list = {"java","c","python","R"};
+
+	public String[] getList() {
+		return list;
+	}
+
+	public void setList(String[] list) {
+		this.list = list;
+	}
+	
+}
